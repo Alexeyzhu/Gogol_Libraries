26502a32eb9db751adcb499d9fb1cf250c09f149
Farit Galeev <2799farit.ksvu@gmail.com>
2018-01-21 13:16:08 +0300

    Fix some issues

--- src/Librarians.java
+++ src/Librarians.java
@@ -1,3 +1,81 @@
-public class Librarians extends Users {
+import javax.swing.*;
+import javax.swing.text.MaskFormatter;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+public class Librarians extends Users {
+    private JTextField status = new JTextField("", 5);
+    private JTextField name = new JTextField("", 5);
+    private JTextField sname = new JTextField("", 5);
+    private JTextField address = new JTextField("", 5);
+    private JFormattedTextField phone;
+    private JLabel lab0 = new JLabel("Status: ");
+    private JLabel lab1 = new JLabel("Name:");
+    private JLabel lab2 = new JLabel("Surname:");
+    private JLabel lab3 = new JLabel("Adress:");
+    private JLabel lab4 = new JLabel("Phone number:");
+    private JButton button = new JButton("Ok");
+    private JButton cancel = new JButton("Cancel");
+    private String[] items = {"Librarian", "Faculty", "Student"};
+    private JComboBox list = new JComboBox(items);
+
+    Librarians() {
+        super();
+        this.setBounds(0, 0, 640, 420);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Container container = this.getContentPane(); //field for buttons;
+        container.setLayout(new GridLayout(6, 0, 2, 2));
+
+        container.add(lab0);
+        container.add(list);
+
+        container.add(lab1);
+        container.add(name);
+
+        container.add(lab2);
+        container.add(sname);
+
+        container.add(lab3);
+        container.add(address);
+
+        container.add(lab4);
+        try {
+            MaskFormatter phoneFormatter = new MaskFormatter("+#-###-###-##-##");
+            phoneFormatter.setPlaceholderCharacter('_');
+            phone = new JFormattedTextField(phoneFormatter);
+            phone.setColumns(16);
+            container.add(phone);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        button.addActionListener(new Event());
+        container.add(button, BorderLayout.EAST);
+
+        cancel.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+                new LogIn().setVisible(true);
+            }
+        });
+        container.add(cancel);
+    }
+
+    protected class Event implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String message = "Status: " + list.getSelectedItem() + "\n";
+            message += "Name: " + name.getText() + "\n";
+            message += "Surname: " + sname.getText() + "\n";
+            message += "Address: " + address.getText() + "\n";
+            message += "Phone: " + phone.getText();
+            JOptionPane.showMessageDialog(null, message, "User Card", JOptionPane.PLAIN_MESSAGE);
+
+        }
+    }
 }
--- src/LogIn.java
+++ src/LogIn.java
@@ -7,24 +7,27 @@ public class LogIn extends JFrame {
 
     private JLabel llab = new JLabel("LogIn");
     private JLabel plab = new JLabel("Password");
+    private JLabel status = new JLabel("Status");
     private JPasswordField pass = new JPasswordField("", 5);
     private JTextField login = new JTextField("", 5);
     private JButton ok = new JButton("Ok");
     private JButton cancel = new JButton("Cancel");
-    static Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-    public static int sizeWidth = 800;
-    public static int sizeHeight = 600;
-    public static int X = (screenSize.width - sizeWidth) / 2;
-    public static int Y = (screenSize.height - sizeHeight) / 2;
+    private String[] items = {"Librarian", "Faculty", "Student"};
+    private JComboBox list = new JComboBox(items);
+
 
     LogIn() {
 
         super("GLib");
-        this.setBounds(X, Y, 320, 250);
+        this.setBounds(0, 0, 320, 320);
+        this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         Container container = this.getContentPane();
-        container.setLayout(new GridLayout(6, 6, 2, 2));
+        container.setLayout(new GridLayout(8, 6, 2, 2));
+
+        container.add(status);
+        container.add(list);
 
         container.add(llab);
         container.add(login);
@@ -36,25 +39,55 @@ public class LogIn extends JFrame {
         container.setVisible(true);
 
         ok.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (isExist()) {
-                    Users user = new Librarians();
-                    user.setVisible(true);
-                    setVisible(false);
-                }
-            }
 
-            private boolean isExist() {
-                return true;               //Здесь должно быть обращение к БД
-            }
-        });
+                                 @Override
+                                 public void actionPerformed(ActionEvent e) {
+                                     Exist();
+                                 }
+
+                                 private void Exist() {
+                                     if (isLibrarian() && login.getText().equals("admin") && pass.getSelectedText().equals("admin")) {
+                                         Users user = new Librarians();
+                                         user.setVisible(true);
+                                         setVisible(false);
+                                     } else if (isFaculty() && login.getText().equals("faculty") && pass.getSelectedText().equals("faculty")) {
+
+                                     } else if (isStudent() && login.getText().equals("student") && pass.getSelectedText().equals("student")) {
+
+                                     } else JOptionPane.showMessageDialog(null, "Incorrect Status, LogIn or Password");
+
+
+                                 }
+
+                                 private boolean isStudent() {
+                                     if (list.getSelectedItem().equals("Student")) {
+                                         return true;
+                                     }
+                                     return false;
+                                 }                                                      //Обращение к БД и проверка, кто есть кто
+
+                                 private boolean isFaculty() {
+                                     if (list.getSelectedItem().equals("Faculty")) {
+                                         return true;
+                                     }
+                                     return false;
+                                 }
+
+                                 private boolean isLibrarian() {
+                                     if (list.getSelectedItem().equals("Librarian")) {
+                                         return true;
+                                     }
+                                     return false;
+                                 }
+
+                             }
+        );
         container.add(ok);
 
         cancel.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                    System.exit(0);
+                System.exit(0);
             }
         });
         container.add(cancel);
