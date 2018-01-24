c805fc3b9bc1f5997972e4d696d82ca887716d62
Алексей <alieksiei.zhuchkov.1999@mil.ru>
2018-01-24 16:29:13 +0300

    change DB settings

--- src/Main.java
+++ src/Main.java
@@ -6,22 +6,35 @@ public class Main {
         String userName = "root";
         String password = "1234";
         String connectionUrl = "jdbc:mysql://localhost:3306/mysql?autoReconnect=true&useSSL=false";
-        Class.forName("com.mysql.jdbc.Driver");
 
-        try (Connection connection = DriverManager.getConnection(connectionUrl, userName, password);
-             Statement statement = connection.createStatement()) {
-            statement.executeUpdate("DROP TABLE books");
-            statement.executeUpdate("CREATE TABLE books (id mediumint NOT NULL auto_increment, " +
-                    "name VARCHAR (30),img LONGBLOB,PRIMARY KEY (id));");
-            statement.executeUpdate("INSERT INTO books (name) VALUES ('Terminal')");
-            statement.executeUpdate("INSERT INTO books SET name = 'Telegram'");
-            statement.executeUpdate("INSERT INTO books SET name = 'Telegram'");
-            ResultSet resultSet = statement.executeQuery("SELECT *FROM books WHERE id = '1'");
-            while (resultSet.next()) {
-                //   System.out.println(resultSet.getInt(1));
-                System.out.println(resultSet.getString(2));
-                System.out.println("--------");
-            }
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("Driver loaded successfully");
+        } catch (Exception e) {
+            System.out.println("Unable to load driver");
+        }
+
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(connectionUrl, userName, password);
+            System.out.println("DataBase connection successfully ");
+        } catch (Exception e) {
+            System.out.println("DB connection Error");
+        }
+
+        Statement statement = connection.createStatement();
+        statement.executeUpdate("DROP TABLE books");
+        statement.executeUpdate("CREATE TABLE books (id mediumint NOT NULL auto_increment, " +
+                "name VARCHAR (30),img LONGBLOB,PRIMARY KEY (id));");
+        statement.executeUpdate("INSERT INTO books (name) VALUES ('Terminal')");
+        statement.executeUpdate("INSERT INTO books SET name = 'Telegram'");
+        statement.executeUpdate("INSERT INTO books SET name = 'Telegram'");
+        ResultSet resultSet = statement.executeQuery("SELECT *FROM books WHERE id = '1'");
+        while (resultSet.next()) {
+            //   System.out.println(resultSet.getInt(1));
+            System.out.println(resultSet.getString(2));
+            System.out.println("--------");
+
 
             LogIn logIn = new LogIn();
             logIn.setVisible(true);
