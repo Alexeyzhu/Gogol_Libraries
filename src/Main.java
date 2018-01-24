11eb43dba91f26414d9734f203d3998ba5e64b90
Алексей <alieksiei.zhuchkov.1999@mil.ru>
2018-01-24 14:37:47 +0300

    change DB settings

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,15 @@ public class Main {
 
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         String userName = "root";
-        String password = "031943";
+        String password = "1234";
         String connectionUrl = "jdbc:mysql://localhost:3306/mysql?autoReconnect=true&useSSL=false";
         Class.forName("com.mysql.jdbc.Driver");
 
         try (Connection connection = DriverManager.getConnection(connectionUrl, userName, password);
              Statement statement = connection.createStatement()) {
-              statement.executeUpdate("DROP TABLE books");
-            statement.executeUpdate("CREATE TABLE books (id mediumint NOT NULL auto_increment, name VARCHAR (30),img LONGBLOB,PRIMARY KEY (id));");
+            statement.executeUpdate("DROP TABLE books");
+            statement.executeUpdate("CREATE TABLE books (id mediumint NOT NULL auto_increment, " +
+                    "name VARCHAR (30),img LONGBLOB,PRIMARY KEY (id));");
             statement.executeUpdate("INSERT INTO books (name) VALUES ('Terminal')");
             statement.executeUpdate("INSERT INTO books SET name = 'Telegram'");
             statement.executeUpdate("INSERT INTO books SET name = 'Telegram'");
