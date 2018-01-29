e54d35943eb3272e5b19946f5baf08d91d17a902
Farit Galeev <f.galeev@innopolis.ru>
2018-01-29 13:43:26 +0300

    New GUI

---
+++ Project/src/project/project.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project;
+
+/**
+ *
+ * @author Farit Galeev
+ */
+public class project {
+   
+    public static void main(String[] args){
+        LogIn logIn = new LogIn();
+        //Librarian lib = new Librarian();
+        //lib.setVisible(true);
+        logIn.setLocationRelativeTo(null);
+        logIn.setVisible(true);
+       //  new Librarian().setVisible(true);   
+    }
+    
+}
