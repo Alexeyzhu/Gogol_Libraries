bfd546a45095e2ff7c653592a2fd48c58f9383c9
imazan <i.mazan@innopolis.ru>
2018-01-30 21:40:26 +0300

    Update Login-Password (how they create)

--- src/Users.java
+++ src/Users.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 
 public class Users extends JFrame {
 
-    public String[] createPerson(String name, String surname, String address, int phone, String type) {
+    public String[] createPerson(String name, String surname, String address, int phone, String type) throws SQLException {
 
 
         return generateLogin(name, surname);
@@ -20,26 +20,29 @@ public class Users extends JFrame {
 
         // create login and password
         String login = name.substring(0, 1).toLowerCase() + "." + surname.toLowerCase();
-        isLogin(login);
         String password = Integer.toString(hashFunction(name, surname));
-        String[] loginData = new String[2];
-        loginData[0] = login;
-        loginData[1] = password;
-        System.out.println(login);
-        System.out.println(password);
+        String[] loginData = {login, password};
+        printLoginData(loginData);
+
         return loginData;
     }
 
+    // delete it later
+    private void printLoginData(String[] loginData) {
+        System.out.println(loginData[0] + "\n" + loginData[1]);
+    }
+
     private int hashFunction(String name, String surname) {
+        final int MAX_PASSWORD_LENGTH = 8;
+        final int BASE_OF_ARITHMETIC = 10;
+
         String data = name + surname;
         while (data.length() < 10) {
             data += data;
         }
-        int hash = data.hashCode();
-        if (hash < 0) {
-            hash = hash * -1;
-        }
-        return hash;
+        int hash = Math.abs(data.hashCode());
+
+        return (int) (hash % Math.pow(BASE_OF_ARITHMETIC, MAX_PASSWORD_LENGTH));
     }
 
     Users() {
