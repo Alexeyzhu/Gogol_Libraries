d2193fba80b5c49d862e849bfe676d66da3ad7e0
imazan <i.mazan@innopolis.ru>
2018-02-01 22:32:57 +0300

    Add exception signature

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,22 @@
-import org.json.JSONObject;
-
-import java.sql.*;
+import javax.management.InstanceAlreadyExistsException;
+import java.sql.SQLException;
 
 public class Main {
 
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException, InstanceAlreadyExistsException {
         Documents documents = new Documents();
         System.out.println(documents.getDocType(2));
-        // Booking booking = new Booking();
-        // booking.checkOut(1, 2);
-        //Users users = new Users();
-        //Librarians librarians = new Librarians();
-        // librarians.addPerson("Dan", "Zhuchkov", "Innopolis", 123456789, "L");
-        // System.out.println(users.checkLoginPassword("answer : " + "n.zhuchkov","305155464"));
-        //System.out.println(users.getType("a.zhuchkov","305129464"));
+        Booking booking = new Booking();
+        booking.checkOut(1, 2);
+        Users users = new Users();
+        Librarians librarians = new Librarians();
+        librarians.addPerson("Dan", "Zhuchkov", "Innopolis", 123456789, "L");
+        System.out.println(users.checkLoginPassword("answer : " + "n.zhuchkov", "305155464"));
+        System.out.println(users.getType("a.zhuchkov", "305129464"));
 
 
-        //  LogIn logIn = new LogIn();
-        //  logIn.setVisible(true);
+        LogIn logIn = new LogIn();
+        logIn.setVisible(true);
     }
 
 
