30ef9dfdfcf7a61d94da40494bdd4ff4998a408f
Алексей <alieksiei.zhuchkov.1999@mil.ru>
2018-02-02 21:36:43 +0300

    add journal articles and AVs

--- src/AV.java
+++ src/AV.java
@@ -3,4 +3,22 @@ import java.sql.SQLException;
 public class AV extends Documents {
     AV() throws SQLException {
     }
+
+    public void addAV(String title, String author, String shelf,
+                        boolean canCheckout) throws SQLException {
+        int id_AV = 0;
+
+        statement.executeUpdate("INSERT INTO av (author, title) " +
+                "VALUES ('" + author + "','" + title + "') ");
+        resultSet = statement.executeQuery("SELECT id FROM av " +
+                "WHERE author = '" + author + "' AND title = '" + title + "'");
+
+        while (resultSet.next()) {
+            id_AV = resultSet.getInt("id");
+        }
+
+        statement.executeUpdate("INSERT INTO documents (id_av,shelf, canCheckout)" +
+                "VALUES (" + id_AV + ",'" + shelf + "'," + canCheckout + ")");
+
+    }
 }
