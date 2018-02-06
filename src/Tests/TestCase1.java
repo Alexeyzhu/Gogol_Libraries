fbc051e78393f79551dae58e05dafe1d157ab5fc
imazan <i.mazan@innopolis.ru>
2018-02-06 23:50:53 +0300

    Create test package

---
+++ src/Tests/TestCase1.java
@@ -0,0 +1,17 @@
+package Tests;
+
+import javax.management.InstanceAlreadyExistsException;
+import java.sql.SQLException;
+
+public class TestCase1 {
+
+    public static void main(String[] args) throws SQLException, InstanceAlreadyExistsException {
+        Librarians lib = new Librarians();
+        Documents documents = new Documents();
+        Patrons patrons = new Patrons();
+        Booking booking = new Booking();
+
+//        booking.checkOut(patrons.getID("f.galeev","45640902"),1, documents.getDocType(1));
+//        lib.getBookedDoc();
+    }
+}
