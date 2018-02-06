358fdf72e3c1e726871aa79545f4aff8788297a1
imazan <i.mazan@innopolis.ru>
2018-02-06 11:38:23 +0300

    Modify code style

--- src/AV.java
+++ src/AV.java
@@ -14,7 +14,7 @@ public class AV extends Documents {
      */
     public void addAV(String title, String author, String shelf,
                         boolean canCheckout) throws SQLException {
-        int id_AV = 0;
+        int idAV = 0;
 
         statement.executeUpdate("INSERT INTO av (author, title) " +
                 "VALUES ('" + author + "','" + title + "') ");
@@ -22,37 +22,43 @@ public class AV extends Documents {
                 "WHERE author = '" + author + "' AND title = '" + title + "'");
 
         while (resultSet.next()) {
-            id_AV = resultSet.getInt("id");
+            idAV = resultSet.getInt("id");
         }
 
         statement.executeUpdate("INSERT INTO documents (id_av,shelf, canCheckout)" +
-                "VALUES (" + id_AV + ",'" + shelf + "'," + canCheckout + ")");
+                "VALUES (" + idAV + ",'" + shelf + "'," + canCheckout + ")");
 
     }
 
     public static int getAVID(int idDoc) throws SQLException {
-        int idAV = 0;
+        final int AV_DOES_NOT_EXIST = 0;
+
+        int idAV = AV_DOES_NOT_EXIST;
         resultSet = statement.executeQuery("SELECT id_av FROM documents " +
                 "WHERE id ='" + idDoc + "'");
+
         while (resultSet.next()) {
             idAV = resultSet.getInt("id_av");
-            System.out.println(idAV);
+            System.out.println("The AV id = " + idAV);
         }
-        if (idAV == 0) {
+
+        if (idAV == AV_DOES_NOT_EXIST) {
             throw new NullPointerException("Something going wrong. This is not a AV.");
         }
+
         return idAV;
     }
 
     public static String[] getAVName(int idDoc) throws SQLException {
         int idAV = getAVID(idDoc);
 
-        String[] name = new String[2];
         resultSet = statement.executeQuery("SELECT * FROM av WHERE id = '" + idAV + "'");
+        String[] name = new String[2];
         while (resultSet.next()){
             name[0] = resultSet.getNString("title");
             name[1] = resultSet.getNString("author");
         }
+
         return name;
     }
 }
